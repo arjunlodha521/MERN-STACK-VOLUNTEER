a7236ea2c52829a0c86fd1d1e88bb14134a4eae5
arjunlodha521 <arjunlodha521@gmail.com>
2024-09-04 14:14:34 +0530

    next commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
+
+
 import app from "./index.js";
 
+//i made changes due to unwanted error of the nodemon
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server Listening At Port ${process.env.PORT}`);
+// });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listening At Port ${process.env.PORT}`);
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
